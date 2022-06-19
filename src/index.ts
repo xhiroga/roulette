72b174084f5c0273cf8fce95b7c66d32e4c76df3
Hiroaki Ogasawara <hiroga1030@gmail.com>
2022-06-19 17:06:54 +0900

    feat: draw circle

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("hi");
+const drawRoulette = (): void => {
+    const canvas = <HTMLCanvasElement>document.getElementById("roulette");
+    Edge(canvas);
+    Roulette(canvas);
+}
+
+const Edge = (canvas: HTMLCanvasElement) => {
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    const path = new Path2D();
+    path.arc(400, 400, 400, 0, 2 * Math.PI);
+    ctx.fillStyle = "green";
+    ctx.fill(path);
+}
+
+const Roulette = (canvas: HTMLCanvasElement) => {
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    const path = new Path2D();
+    path.arc(400, 400, 380, 0, 2 * Math.PI);
+    ctx.fillStyle = "white";
+    ctx.fill(path);
+}
+
+document.addEventListener("DOMContentLoaded", drawRoulette);
