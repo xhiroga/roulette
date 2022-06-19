db42834dcf85facd864b74ec4309f63ab16e296b
Hiroaki Ogasawara <hiroga1030@gmail.com>
2022-06-19 17:26:20 +0900

    chore: linter and formatter

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
 const drawRoulette = (): void => {
-    const canvas = <HTMLCanvasElement>document.getElementById("roulette");
-    Edge(canvas);
-    Roulette(canvas);
+  const canvas = <HTMLCanvasElement>document.getElementById('roulette')
+  Edge(canvas)
+  Roulette(canvas)
 }
 
 const Edge = (canvas: HTMLCanvasElement) => {
-    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-    const path = new Path2D();
-    path.arc(400, 400, 400, 0, 2 * Math.PI);
-    ctx.fillStyle = "green";
-    ctx.fill(path);
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+  const path = new Path2D()
+  path.arc(400, 400, 400, 0, 2 * Math.PI)
+  ctx.fillStyle = 'green'
+  ctx.fill(path)
 }
 
 const Roulette = (canvas: HTMLCanvasElement) => {
-    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-    const path = new Path2D();
-    path.arc(400, 400, 380, 0, 2 * Math.PI);
-    ctx.fillStyle = "white";
-    ctx.fill(path);
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+  const path = new Path2D()
+  path.arc(400, 400, 380, 0, 2 * Math.PI)
+
+  ctx.fillStyle = 'white'
+  ctx.fill(path)
 }
 
-document.addEventListener("DOMContentLoaded", drawRoulette);
+document.addEventListener('DOMContentLoaded', drawRoulette)
